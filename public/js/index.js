9ea01737f7737af6180bab92d5f5a41227886047
LuisRetana <luisretana123@gmail.com>
2021-11-25 08:07:29 -0500

    added createtranaction function

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,33 @@
+//added service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then((reg) => {
+      console.log("Service worker registered.", reg);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
+const transactionForm = createTransactionForm();
+const transactionApi = createTransactionApi();
+
+initTransactions();
+
+document.querySelector("#add-btn").onclick = function () {
+  sendTransaction(true);
+};
+
+document.querySelector("#sub-btn").onclick = function () {
+  sendTransaction(false);
+};
+
+function createTransactionForm() {
+  const nameEl = document.querySelector("#t-name");
+  const amountEl = document.querySelector("#t-amount");
+  const errorEl = document.querySelector(".form .error");
+
+  const showError = (message) => {
+    errorEl.textContent = message;
+  };
 
