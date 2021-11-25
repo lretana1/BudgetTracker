f42de14c5beb58488c522bc9d463d5237fba8532
LuisRetana <luisretana123@gmail.com>
2021-11-25 09:07:11 -0500

    created clear function, POST, fetch all request

--- public/js/index.js
+++ public/js/index.js
@@ -31,3 +31,72 @@ function createTransactionForm() {
     errorEl.textContent = message;
   };
 
+  const validate = () => {
+    if (nameEl.value === "" || amountEl.value === "") {
+      showError("Missing Information");
+      return false;
+    }
+    showError("");
+    return true;
+  };
+
+  const transaction = () => {
+    return {
+      name: nameEl.value,
+      value: amountEl.value,
+      date: new Date().toISOString()
+    };
+  };
+
+  // clears inputs
+  const clear = () => {
+    nameEl.value = "";
+    amountEl.value = "";
+    showError("");
+  };
+
+  return Object.freeze({ transaction, validate, clear, showError });
+}
+
+function createTransactionApi() {
+  const create = (transaction) => {
+    return fetch("/api/transaction", {
+      method: "POST",
+      body: JSON.stringify(transaction),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      return response.json();
+    });
+  };
+
+  const fetchAll = () => {
+    return fetch("/api/transaction").then((response) => {
+      return response.json();
+    });
+  };
+  return Object.freeze({ create, fetchAll });
+}
+
+function initTransactions() {
+  transactionApi.fetchAll().then((data) => {
+    // save db data on global variable
+    transactions = data;
+
+    renderTransactionsChart();
+  });
+}
+
+function sendTransaction(isAdding) {
+  if (!transactionForm.validate()) {
+    return;
+  }
+
+  const transaction = transactionForm.transaction();
+
+  // Converts to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
